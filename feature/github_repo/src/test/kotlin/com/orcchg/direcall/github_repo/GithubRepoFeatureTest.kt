af126c313bc1c8ff69f5620d55e26492b2f1dade
Alexander Mitropolskiy <AAMitropolskiy@sberbank.ru>
2022-05-22 22:28:43 +0300

    Демо приложение

--- feature/github_repo/src/test/kotlin/com/orcchg/direcall/github_repo/GithubRepoFeatureTest.kt
+++ feature/github_repo/src/test/kotlin/com/orcchg/direcall/github_repo/GithubRepoFeatureTest.kt
@@ -3,12 +3,15 @@ package com.orcchg.direcall.github_repo
 import com.orcchg.direcall.apptest.checkDynamicFeatureApi
 import com.orcchg.direcall.features
 import com.orcchg.direcall.github_repo.di.GithubRepoFeatureApi
+import com.orcchg.direcall.github_repo.presentation.viewmodel.GithubRepoViewModel
 import org.junit.Test
 
 class GithubRepoFeatureTest {
 
     @Test
     fun `check github repo feature`() {
-        checkDynamicFeatureApi(features(), GithubRepoFeatureApi)
+        checkDynamicFeatureApi(features(), GithubRepoFeatureApi) {
+            withParameter<GithubRepoViewModel> { "login" }
+        }
     }
 }
